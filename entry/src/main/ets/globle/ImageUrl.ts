2db18c3b8a7f13ef81e6c7611358664777fe3e11
李锦涛 <lijt@eetrust.com>
2024-03-10 17:47:29 +0800

    commit(master):List

---
+++ entry/src/main/ets/globle/ImageUrl.ts
@@ -0,0 +1,10 @@
+/**
+ * Desc:
+ * @author lijt
+ * Created on {2024/3/9}
+ * Email: lijt@eetrust.com
+ */
+export class ImageUrl{
+  static readonly URL1 = 'https://img2.huashi6.com/images/resource/thumbnail/2022/10/15/154533_93334186487.jpg?imageMogr2/quality/100/interlace/1/thumbnail/2000x%3E'
+  static readonly URL2 = 'https://ts1.cn.mm.bing.net/th/id/R-C.5d0c289d1219b6663b9432bcb3fd96d4?rik=mVNpEwZk0wHdEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f017b1b57f455c7a84a0e282bee54c3.jpg%403000w_1l_2o_100sh.jpg&ehk=8rEwMuLV1vuG7ibHvrC0ygdapp6NPMSc1Fu5irZqD40%3d&risl=&pid=ImgRaw&r=0'
+}
